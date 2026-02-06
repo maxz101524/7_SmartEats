516ffab91030d52814b907a82cbebe0430ca31c7
toondir <matumc@hotmail.com>
2026-02-06 17:54:18 -0600

    add pages

--- backend/SmartEats_config/settings/base.py
+++ backend/SmartEats_config/settings/base.py
@@ -28,9 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
 
-ALLOWED_HOSTS = []
 
 
 # Application definition
